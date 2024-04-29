20f8464c2503c90af99ccb11a3260f1e548cc69f
Salah AlDin <salahqerem@gmail.com>
2024-04-30 01:52:25 +0300

    Paginate todos

--- Frontend/src/components/Todos/Todos.jsx
+++ Frontend/src/components/Todos/Todos.jsx
@@ -1,29 +1,44 @@
-import { Box, Grid, TextField } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Box,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Pagination,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import Loader from "../Loader";
 import Todo from "./components/Todo.jsx";
-import useGetTodos from "./hooks/useGetTodos.js";
 import { LoadingButton } from "@mui/lab";
 import useAddTodos from "./hooks/useAddTodo.js";
 import { CustomSuccessAlert } from "../../utils/general.js";
-import useUpdateTodo from "./hooks/useUpdateTodo.js";
+import useGetTodos from "./hooks/useGetTodos.js";
 
 const Todos = () => {
   let [todos, setTodos] = useState([]);
   let [newTodoTitle, setNewTodoTitle] = useState("");
   let [newTodoDescription, setNewTodoDescription] = useState("");
+  let [page, setPage] = useState(1);
+  let [limit, setLimit] = useState(5);
+  let [numOfPages, setNumOfPages] = useState(10);
 
-  const { isFetchingTodos } = useGetTodos(setTodos);
+  const { fetchTodos, isFetchingTodos } = useGetTodos(setTodos, setNumOfPages);
   const { addTodo, isAddingTodo, isAdded } = useAddTodos(setTodos);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     await addTodo({ title: newTodoTitle, description: newTodoDescription });
-    if (isAdded) {
-      setNewTodoDescription("");
-      setNewTodoTitle("");
-      CustomSuccessAlert("New Todo added successfully");
-    }
+  };
+
+  const handlePageChange = (e, value) => {
+    setPage(value);
+  };
+
+  const handleLimitChange = (e, { props }) => {
+    const limit = props.value;
+    setLimit(limit);
   };
 
   const isValidateInputs =
@@ -35,69 +50,155 @@ const Todos = () => {
     </Grid>
   ));
 
+  useEffect(() => {
+    if (isAdded) {
+      setNewTodoDescription("");
+      setNewTodoTitle("");
+      CustomSuccessAlert("New Todo added successfully");
+    }
+    fetchTodos(page, limit);
+  }, [isAdded, page, limit]);
+
   if (isFetchingTodos) return <Loader />;
   return (
     <Box>
       <Box
-        component="form"
         sx={{
-          width: "76.5%",
           margin: "2rem auto",
+          width: "76.5%",
           display: "flex",
           alignItems: "flex-start",
-          gap: 4,
-          height: "70px",
+          justifyContent: "space-between",
+          gap: 10,
         }}
-        noValidate
-        autoComplete="off"
-        onSubmit={handleSubmit}
       >
-        <TextField
-          id="title"
-          name="title"
-          label="Todo Title"
-          variant="outlined"
-          value={newTodoTitle}
-          onChange={(e) => setNewTodoTitle(e.target.value)}
-          error={newTodoTitle.length > 0 && newTodoTitle.length < 10}
-          helperText={
-            newTodoTitle.length > 0 && newTodoTitle.length < 10
-              ? "Title must be at least 10 characters"
-              : ""
-          }
-          sx={{ width: "30%" }}
-        />
-        <TextField
-          id="description"
-          name="description"
-          label="Todo Description"
-          variant="outlined"
-          value={newTodoDescription}
-          onChange={(e) => setNewTodoDescription(e.target.value)}
-          error={
-            newTodoDescription.length > 0 && newTodoDescription.length < 15
-          }
-          helperText={
-            newTodoDescription.length > 0 && newTodoDescription.length < 15
-              ? "Description must be at least 15 characters"
-              : ""
-          }
-          sx={{ flexGrow: 1 }}
-        />
-        <LoadingButton
-          loading={isAddingTodo}
-          variant="contained"
-          size="large"
-          type="submit"
-          disabled={isValidateInputs}
-          sx={{ p: "14px" }}
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            flexGrow: 1,
+            height: "70px",
+            gap: 4,
+          }}
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
         >
-          Add Todo
-        </LoadingButton>
+          <TextField
+            id="title"
+            name="title"
+            label="Todo Title"
+            variant="outlined"
+            value={newTodoTitle}
+            onChange={(e) => setNewTodoTitle(e.target.value)}
+            error={newTodoTitle.length > 0 && newTodoTitle.length < 10}
+            helperText={
+              newTodoTitle.length > 0 && newTodoTitle.length < 10
+                ? "Title must be at least 10 characters"
+                : ""
+            }
+            sx={{
+              width: "30%",
+            }}
+          />
+          <TextField
+            id="description"
+            name="description"
+            label="Todo Description"
+            variant="outlined"
+            value={newTodoDescription}
+            onChange={(e) => setNewTodoDescription(e.target.value)}
+            error={
+              newTodoDescription.length > 0 && newTodoDescription.length < 15
+            }
+            helperText={
+              newTodoDescription.length > 0 && newTodoDescription.length < 15
+                ? "Description must be at least 15 characters"
+                : ""
+            }
+            sx={{
+              flexGrow: 1,
+            }}
+          />
+          <LoadingButton
+            loading={isAddingTodo}
+            variant="contained"
+            size="large"
+            type="submit"
+            disabled={isValidateInputs}
+            sx={{
+              p: "14px",
+              boxShadow:
+                "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+            }}
+          >
+            Add Todo
+          </LoadingButton>
+        </Box>
+        <Box
+          sx={{
+            maxWidth: 100,
+          }}
+        >
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Limit</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={limit}
+              label="limit"
+              onChange={handleLimitChange}
+              sx={{
+                height: "56px",
+                textAlign: "center",
+                width: "100px",
+              }}
+              MenuProps={{
+                anchorOrigin: {
+                  vertical: "bottom",
+                  horizontal: "left",
+                },
+                transformOrigin: {
+                  vertical: "top",
+                  horizontal: "left",
+                },
+              }}
+            >
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={15}>15</MenuItem>
+              <MenuItem value={20}>20</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
       </Box>
-      <Grid container spacing={4} width={"80%"} sx={{ margin: "1rem auto" }}>
+      <Grid
+        container
+        spacing={4}
+        width={"80%"}
+        sx={{ marginX: "auto", paddingBlockEnd: 15 }}
+      >
         {renderTodos}
       </Grid>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          position: "fixed",
+          left: 0,
+          right: 0,
+          bottom: 0,
+          py: 3,
+          backgroundColor: "white",
+        }}
+      >
+        <Pagination
+          count={numOfPages}
+          page={page}
+          onChange={handlePageChange}
+        />
+      </Box>
     </Box>
   );
 };
--- Frontend/src/components/Todos/hooks/useGetTodos.js
+++ Frontend/src/components/Todos/hooks/useGetTodos.js
@@ -1,22 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-const useGetTodos = (setTodos) => {
+const useGetTodos = (setTodos, setNumOfPages) => {
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchTodos = async () => {
-      const response = await fetch(
-        "https://fullstack-todolist-upnv.onrender.com/todos?page=1&limit=1000"
-      );
-      const data = await response.json();
-      setTodos(data.todos);
-      setIsLoading(false);
-    };
+  const fetchTodos = async (page, limit) => {
+    const response = await fetch(
+      `https://fullstack-todolist-upnv.onrender.com/todos?page=${page}&limit=${limit}`
+    );
+    const data = await response.json();
+    setTodos(data.todos);
+    setNumOfPages(data.numOfPages);
+    setIsLoading(false);
+  };
 
-    fetchTodos();
-  }, []);
-
-  return { isFetchingTodos: isLoading };
+  return { fetchTodos, isFetchingTodos: isLoading };
 };
 
 export default useGetTodos;
