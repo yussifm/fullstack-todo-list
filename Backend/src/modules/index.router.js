ccc179f288d08933d4de4f1580b3e2c5df757903
Salah AlDin <salahqerem@gmail.com>
2024-04-29 17:10:42 +0300

    Setup api with the database

---
+++ Backend/src/modules/index.router.js
@@ -0,0 +1,16 @@
+import connectDb from "../../DB/connection.js";
+// import noteRouter from "./notes/note.router.js";
+import cors from "cors";
+
+const initApp = async (app, express) => {
+  const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200,
+  };
+  connectDb();
+  app.use(cors(corsOptions));
+  app.use(express.json());
+  //   app.use("/notes", noteRouter);
+};
+
+export default initApp;
